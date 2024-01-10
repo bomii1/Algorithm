31cffe2c78dc9bd4c4ea020a46abb4123b774f51
Jeong Bomi <101382369+bomii1@users.noreply.github.com>
2024-01-11 00:13:52 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,53 @@
+import sys
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, val):
+        self.items.append(val)
+
+    def pop(self):
+        try:
+            print(self.items[0])
+            self.items.pop(0)
+        except IndexError:
+            print(-1)
+
+    def front(self):
+        try:
+            print(self.items[0])
+        except IndexError:
+            print(-1)
+    def back(self):
+        try:
+            print(self.items[-1])
+        except IndexError:
+            print(-1)
+    def empty(self):
+        if len(self.items) == 0:
+            print(1)
+        else:
+            print(0)
+
+    def size(self):
+        print(len(self.items))
+
+s = Stack()
+
+n = int(sys.stdin.readline())
+stackList = [sys.stdin.readline().strip() for i in range(n)]
+
+for i in range(n):
+    if stackList[i][1] == 'u': # push
+        s.push(stackList[i][5:])
+    elif stackList[i][0] == 'p' and stackList[i][1] == 'o': # pop
+        s.pop()
+    elif stackList[i][0] == 'f': # front
+        s.front()
+    elif stackList[i][0] == 'b': # back
+        s.back()
+    elif stackList[i][0] == 's': # size
+        s.size()
+    elif stackList[i][0] == 'e': # empty
+        s.empty()
