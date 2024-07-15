1b156a85a85bd5250d29d571759cd48db65eedcd
Jeong Bomi <101382369+bomii1@users.noreply.github.com>
2024-07-15 17:58:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 128 ms, Memory: 35612 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+tri = []
+for i in range(n):
+    tri.append(list(map(int, input().split())))
+
+for i in range(1, n):
+    for j in range(i+1):
+        if j == 0:
+            tri[i][j] = tri[i-1][0] + tri[i][j]
+        elif j == i:
+            tri[i][j] = tri[i-1][i-1] + tri[i][j]
+        else:
+            tri[i][j] = max(tri[i-1][j-1], tri[i-1][j]) + tri[i][j]
+
+print(max(tri[-1]))
