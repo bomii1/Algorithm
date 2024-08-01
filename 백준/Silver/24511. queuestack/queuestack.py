d7a49432b6ec4609f9a77a1ef6da2f4d194d42f0
Jeong Bomi <101382369+bomii1@users.noreply.github.com>
2024-08-02 02:23:14 +0900

    [Silver III] Title: queuestack, Time: 372 ms, Memory: 47508 KB -BaekjoonHub

---
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+M = int(input())
+C = deque(map(int, input().split()))
+
+inputArr = C
+for i in range(N):
+    if A[i] == 0: # 큐
+        #d = deque()
+        inputArr.appendleft(B[i])
+        inputArr.pop()
+
+
+for i in range(len(inputArr)):
+    print(inputArr[i], end = ' ')
