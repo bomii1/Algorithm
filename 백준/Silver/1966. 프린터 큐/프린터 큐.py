855639862a59702ca3c12778e900fed6d799fce3
Jeong Bomi <101382369+bomii1@users.noreply.github.com>
2024-08-08 15:35:34 +0900

    [Silver III] Title: 프린터 큐, Time: 80 ms, Memory: 34068 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -8,7 +8,6 @@ for _ in range(T):
     N, M = map(int, input().split())
     input_values = list(map(int, input().split()))
     importance = deque([index, value] for index, value in enumerate(input_values))
-    sorted_importance = sorted(importance, key=lambda x: x[1], reverse=True)
     find_value = importance[M]
     count = 0
     sorted_index = 0
