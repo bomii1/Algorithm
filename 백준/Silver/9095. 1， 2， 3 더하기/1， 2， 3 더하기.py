ec3e58628607a15f35c82db36c6d67edd6353b77
Jeong Bomi <101382369+bomii1@users.noreply.github.com>
2026-03-27 14:32:08 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,14 +1,12 @@
-n = int(input())
-
 dp = [0] * 11
 dp[1] = 1
 dp[2] = 2
 dp[3] = 4
 
 for i in range(4, 11):
-    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+  dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
 
-for _ in range(n):
-    x = int(input())
-    print(dp[x])
-
+T = int(input())
+for _ in range(T):
+  n = int(input())
+  print(dp[n])
